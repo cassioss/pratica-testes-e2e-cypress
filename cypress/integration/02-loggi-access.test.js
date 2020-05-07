70df572dbee707ff8bc117ecbe703d78d714357a
cassioss <cassiosantossousa@gmail.com>
2020-05-07 13:29:34 -0300

    Improving tests for Loggi access

--- cypress/integration/02-loggi-access.test.js
+++ cypress/integration/02-loggi-access.test.js
@@ -2,13 +2,19 @@
 
 describe('Loggi access in Staging', () => {
 
-  it('login, input name/password and access Presto', () => {
+  it('goes to /loggin page', () => {
     cy.visit('https://staging.loggi.com/');
     cy.get('a').contains('Entrar').click();
+    cy.url().should('contain', '/loggin');
+  });
+
+  it('inputs email and password', () => {
     cy.get('input[data-e2e="input-email"]').type('teste.e2e@loggi.com');
     cy.get('input[data-e2e="input-password"]').type('loggi');
+  });
+
+  it('logs in successfully and access Presto', () => {
     cy.get('button').contains('Entrar').click();
     cy.url().should('contain', '/presto/app');
   });
-
 });
