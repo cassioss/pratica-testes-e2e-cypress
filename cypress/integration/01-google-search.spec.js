6dde9934432161b669f61d5b57c1b8d116ecc42c
cassioss <cassiosantossousa@gmail.com>
2020-05-07 16:21:05 -0300

    Adding case for search when clicking on button

--- cypress/integration/01-google-search.spec.js
+++ cypress/integration/01-google-search.spec.js
@@ -15,4 +15,9 @@ describe('Google search', () => {
     cy.get('input[role=combobox]').type('Loggi entregador');
     cy.get('li').contains('loggi entregador contato').parent().click();
   });
+
+  it('search when typing on search box and clicking on the Pesquisa Google button', () => {
+    cy.get('input[role=combobox]').type('Loggi entregador hiuergirejgiunoewimiojmi');
+    cy.get('input').contains('Pesquisa Google').click();
+  });
 });
